0296c50b1f3a6d11f5c241a55f166b57c30879c3
DimazAgastya <90527450+DimazAgastya@users.noreply.github.com>
2022-06-25 12:27:05 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Account from "./Pages/Account";
-import Profile from "./Pages/Profile";
-import DaftarJual from "./Pages/DaftarJual";
-import InfoProduk from "./Pages/InfoProduk";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
 
-function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Login />}></Route>
-			<Route path="/register" element={<Register />}></Route>
-			<Route path="/account" element={<Account />}></Route>
-			<Route path="/profile" element={<Profile />}></Route>
-			<Route path="/daftarJual" element={<DaftarJual />}></Route>
-			<Route path="/infoProduk" element={<InfoProduk />}></Route>
-		</Routes>
-	);
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Buyer from './Pages/Buyer'
+import InfoPenawaran from './Pages/InfoPenawaran';
+import Seller from './Pages/Seller'
+
+const App = ()  => {
+
+  return (
+   <div className="app_container">
+    <BrowserRouter>
+     <Routes>
+       <Route path ="/" element={<Buyer/>}/>
+       <Route path ="/info" element={<InfoPenawaran/>}/>
+       <Route path ="/seller" element={<Seller/>}/>
+     </Routes>
+     </BrowserRouter>
+   </div>
+  );
 }
 
 export default App;
